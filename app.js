77ef1d09c3efb2ef720bc980d1c2c62ddcd45d67
Chris Sauter <sauterdev@gmail.com>
2023-01-03 15:25:21 -0500

    operators exercise

--- app.js
+++ app.js
@@ -5,15 +5,66 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
-
 // YOUR CODE HERE
+
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(a, b, add, minus, multiply, dividing)
+
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 
+//What is the value of: num + str? `1111`
+console.log(num + str);
+// What is the value of: num + str2? `11eleven`
+console.log (num + str2);
+// What is the value of: num + isPresent? 12
+console.log(num + isPresent); 
+// What is the value of: firstName + num? `Frodo11`
+console.log (firstName + num);
+// What is the value of: isPresent + str? `true11`
+console.log(isPresent + str);
+// What is the value of: firstName + lastName? `FrodoBaggins`
+console.log(firstName + lastName);
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+// What is the value of: val == str3? true
+console.log(val == str3);
+// What is the value of: val === str3? false
+console.log(val === str3);
+// What is the value of: !isPresent2? true
+console.log(!isPresent2);
+// What is the value of: (“eleven” == str4 && val >= str3)? flase
+console.log(str2 == str4 && val >= str3);
+// What is the value of: (!isPresent2 || isPresent2)? true
+console.log(!isPresent2 || isPresent2);
+// What is the value of: 0 == false? true
+console.log(0 == false);
+// What is the value of: 0 === false? false
+console.log(0 === false);
+// What is the value of: 0 != false? false
+console.log(0 != false);
+// What is the value of 0 !== false? true
+console.log(0 !== false);
